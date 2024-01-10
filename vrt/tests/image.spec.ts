466b6c8841fe2a6159ec8c64adecbb9efbe261ae
swfz <sawafuji.09@gmail.com>
2024-01-10 21:07:18 +0900

    chore: 不要な記述の削除

--- vrt/tests/image.spec.ts
+++ vrt/tests/image.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import fs from 'fs';
 
 test('user params does not exist', async ({ page }) => {
   await page.goto('http://localhost:8080/');
@@ -14,12 +13,6 @@ test('user does not exist', async ({ page }) => {
 });
 
 test('Image', async ({ page }) => {
-  const testData = fs.readFileSync('contributions.json', 'utf8');
-  await page.route('api.github.com', route => route.fulfill({
-    status: 200,
-    body: testData,
-  }));
-
   await page.goto('http://localhost:8080/?user=swfz');
 
   await expect(page).toHaveScreenshot();
